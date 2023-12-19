e8a7cbd73a8cd74af2864d8d4cb6b1ef779385d0
saipraveennam <147487930+saipraveennam@users.noreply.github.com>
2023-12-19 07:08:37 +0530

    #include<stdio.h> int main() {     int n,i,c=0,sum=0;     float a;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     a=float(sum/n);     for(i=0;i<n;i++)     {     if(arr[i]>=a)     {      c=c+1;     }     }     printf("%d",c); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0,sum=0;
+    float a;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    a=float(sum/n);
+    for(i=0;i<n;i++)
+    {
+    if(arr[i]>=a)
+    {
+     c=c+1;  
+    }
+    }
+    printf("%d",c);
+}
